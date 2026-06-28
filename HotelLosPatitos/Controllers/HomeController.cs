2d9085082118b081049b1c23d80947df6801ec9e
StevenGalvezz <cgalvez30381@ufide.ac.cr>
2026-06-27 23:00:06 -0600

    Add project files.

---
+++ HotelLosPatitos/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using HotelLosPatitos.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace HotelLosPatitos.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ HotelLosPatitos/HotelLosPatitos.Domain/Entities/Habitacion.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HotelLosPatitos.Domain.Entities
+{
+    public class Habitacion
+    {
+        public int Id { get; set; }
+        public string CodigoDeHabitacion { get; set; } = null!;
+        public string NombreDeHabitacion { get; set; } = null!;
+        public int CantidadDeHuespedesPermitidos { get; set; }
+        public int CantidadDeCamas { get; set; }
+        public int CantidadDeBanos { get; set; }
+        public string Ubicacion { get; set; } = null!;
+        public string EncargadoDeLimpieza { get; set; } = null!;
+        public int TipoDeHabitacion { get; set; } // 1- Junior, 2- Superior, 3- Suite
+        public decimal CostoDeLimpieza { get; set; }
+        public decimal CostoDeReserva { get; set; }
+        public DateTime FechaDeRegistro { get; set; }
+        public DateTime? FechaDeModificacion { get; set; }
+        public bool Estado { get; set; } // true = Activo, false = Inactivo
+
+        // Propiedad de navegación para la relación (Una habitación tiene muchas reservaciones)
+        public virtual ICollection<Reservacion> Reservaciones { get; set; } = new List<Reservacion>();
+    }
+}
